a1fac76c99606b53695096c4262dc7e5ceefae55
Pham Hieu <phamhieu1998@gmail.com>
2020-09-15 11:19:57 +0800

    chore: add example to use with Supabase api

---
+++ example/main.dart
@@ -0,0 +1,21 @@
+import 'package:postgrest/postgrest.dart';
+
+///
+/// Example to use with Supabase API https://supabase.io/
+///
+void main(List<String> arguments) async {
+  var client = PostgrestClient("SUPABASE_API_ENDPOINT/rest/v1", {
+    'headers': {
+      'apikey': 'SUPABSE_API_KEY',
+    },
+    'schema': 'public'
+  });
+
+  final response =
+      await client.from('countries').select('*').order('name', true).end();
+  if (response['statusCode'] == 200) {
+    print('Countries List: ${response['body']}.');
+  } else {
+    print('Request failed with status: ${response['statusCode']}.');
+  }
+}
