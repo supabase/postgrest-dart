a2295ec675fc0e1aef615beb00834b3f8a0fadf3
Aravind kumar A <asaravindkumar@gmail.com>
2020-10-02 07:42:25 +0530

    fixed linting in all dart files

--- test/filter_test.dart
+++ test/filter_test.dart
@@ -5,8 +5,12 @@ void main() {
   String rootUrl = 'http://localhost:3000';
   var postgrest = PostgrestClient(rootUrl);
 
-  test("not", () async {
-    var res = await postgrest.from('users').select().not('status', 'eq', 'OFFLINE').end();
+  test('not', () async {
+    var res = await postgrest
+        .from('users')
+        .select()
+        .not('status', 'eq', 'OFFLINE')
+        .end();
     expect(res['body'].length, 3);
     expect(res['body'][1]['username'], 'awailas');
   });
--- test/transforms_test.dart
+++ test/transforms_test.dart
@@ -5,8 +5,11 @@ void main() {
   String rootUrl = 'http://localhost:3000';
   var postgrest = PostgrestClient(rootUrl);
 
-  test("order", () async {
-    var res = await postgrest.from('users').select().order('username', {'ascending': false}).end();
+  test('order', () async {
+    var res = await postgrest
+        .from('users')
+        .select()
+        .order('username', {'ascending': false}).end();
     expect(res['body'][1]['username'], 'kiwicopple');
     expect(res['body'][3]['username'], 'awailas');
   });
