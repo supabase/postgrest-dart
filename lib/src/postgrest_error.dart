8ddea1d8caa8b6e0aff42a9fbbd40d7dab06b16c
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-09-06 19:25:42 +0900

    fix: reordered properties of postgrest_error according to importance

--- lib/src/postgrest_error.dart
+++ lib/src/postgrest_error.dart
@@ -2,33 +2,33 @@
 class PostgrestError {
   PostgrestError({
     required this.message,
+    this.code,
     this.details,
     this.hint,
-    this.code,
   });
 
-  String message;
-  dynamic details;
-  String? hint;
-  String? code;
+  final String message;
+  final String? code;
+  final dynamic details;
+  final String? hint;
 
   factory PostgrestError.fromJson(Map<String, dynamic> json) => PostgrestError(
         message: json['message'] as String,
+        code: json['code'] as String?,
         details: json['details'] as dynamic,
         hint: json['hint'] as String?,
-        code: json['code'] as String?,
       );
 
   Map<String, dynamic> toJson() => {
         'message': message,
+        'code': code,
         'details': details,
         'hint': hint,
-        'code': code,
       };
 
   @override
   String toString() {
-    return 'PostgrestError{message: $message, '
-        'details: ${details?.toString()}, hint: $hint, code: $code}';
+    return 'PostgrestError(message: $message, '
+        'code: $code, details: ${details?.toString()}, hint: $hint)';
   }
 }
